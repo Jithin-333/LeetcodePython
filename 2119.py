ab3e8015cb83d0a8a8046c5c379e67bcc52f8332
Jithin S <147032520+Jithin-333@users.noreply.github.com>
2024-12-23 12:25:44 +0530

    Create 2119.py

---
+++ 2119.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def capitalizeTitle(self, title):
+        ls = title.split()
+        out=[]
+        for i in ls:
+            if len(i) <= 2:
+                out.append(i.lower())
+            else:
+                out.append(i.lower().capitalize())
+        return ' '.join(out)
